2f6e34d85554dab4bc5aaee7b4ea231ccd5cdf44
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2020-09-09 11:39:58 +0530

    variables

--- tut1.cpp
+++ tut1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+  //variables
+  int age = 20;
+  char grade = 'A';
+  float pi = 3.1416;
+
   std::cout << "Hello World!" << std::endl;
+  std::cout << "I am " << age << " years old, grade = " << grade << " and PI = " << pi << std::endl; 
   return 0;
 }
